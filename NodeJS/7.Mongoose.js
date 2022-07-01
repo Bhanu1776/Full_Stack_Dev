e9232e787fd9ec9698d7d57777972b0227037179
Bhanu Sunka <sunkabhanu281202@gmail.com>
2022-07-02 00:05:17 +0530

    Mongoose node modules

---
+++ NodeJS/7.Mongoose.js
@@ -0,0 +1,8 @@
+// getting-started.js
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/test');
+}
