3d7018b977d033e94287050e05963353e726aa43
Bhanu Sunka <sunkabhanu281202@gmail.com>
2022-07-05 16:37:54 +0530

    Everything about Components and Props

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,26 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css';                             //* This is the way to import css files in js
+import Navbar from './components/Navbar.js'     //* This is the way to import components in react
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+// From this App.js all components going to load, In short browser will load according to this file
+
+//* What is JSX ?
+// It stands for Javascript XML. It is the syntax extension of Javascript which allows HTML to co-exist with Javascript in React.
+
+//* Naming conventions in JSX.
+// ==> You have to write "className" instead of "class" as class is the reserved keyword in Javascript, In place of "for" you have to use "htmlFor"
+// JS content has to be written under curly braces{}
+// In JSX closing the tags is very imp 
+
+
+function App() {                                //* React uses the Function-based components  
+  return (                                      // Html and js should be return under return func!! Content written under return is known as jsx ..
+    <>                                          {/* Html code has to be written under these brackets only! JSX fragment feature */}
+      
+                                                {/*//* This is the way to write comments in JSX!! Pain 😤*/}
+      
+      <Navbar title="TextUtils" aboutText="AboutUs"/>
+
+    </>
   );
 }
 
-export default App;
+export default App;                             // Exporting App() func to other components..
