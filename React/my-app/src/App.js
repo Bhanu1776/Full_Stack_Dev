ea6d4cc873f0730f75783d882af6f82a93af5ff8
Bhanu Sunka <sunkabhanu281202@gmail.com>
2022-07-07 13:51:27 +0530

    Alerts in react

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,37 +1,36 @@
-import './App.css';                             //* This is the way to import css files in js
-import Navbar from './components/Navbar.js'     //* This is the way to import components in react
+import './App.css';                            
+import Navbar from './components/Navbar.js'    
 import TextForm from './components/TextForm';
 // import About from './components/About';
-import React, { useState } from 'react';        // Using hooks in main app.js
+import React, { useState } from 'react';       
+import Alert from './components/Alert';
 
-// From this App.js all components going to load, In short browser will load according to this file
+function App(props) {
 
-//* What is JSX ?
-// It stands for Javascript XML. It is the syntax extension of Javascript which allows HTML to co-exist with Javascript in React.
+  const [mode, setMode] = useState('light');     
+  const [alert, setAlert] = useState(null);            
 
-//* Naming conventions in JSX.
-// ==> You have to write "className" instead of "class" as class is the reserved keyword in Javascript, In place of "for" you have to use "htmlFor"
-// JS content has to be written under curly braces{}
-// In JSX closing the tags is very imp, which aren't closed by default
-// Do check console frequently in browser to debug errors..
-
-//* Adding components in JS
-// First thing, Import component 
-// Include component in App func
-
-
-function App() {
-
-  const [mode, setMode] = useState('light');                    // Whether dark mode is enabled or not, By default is Light mode
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+        setAlert(null);
+    }, 1500);
+}
 
   const toggleMode = ()=>{
-    if(mode === 'light'){                                       // When we toggle the switch, Light mode becomes Dark mode and vice-versa
+    if(mode === 'light'){                                    
       setMode('dark');
-      document.body.style.backgroundColor = '#042743';          // This way we can change the body bg color
+      document.body.style.backgroundColor = '#042743';    
+      showAlert("Dark mode has been enabled", "success");    
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+
     }
   }
 
@@ -39,10 +38,11 @@ function App() {
     <>
       <Navbar title="TextUtils" aboutText="AboutUs" mode={mode} toggleMode={toggleMode}/>
 
+      <Alert alert={alert}/>
       {/* <About/> */}
-      <TextForm mode={mode}/>
+      <TextForm mode={mode} showAlert={showAlert}/>
     </>
   );
 }
 
-export default App;                             // Exporting App() func to other components..
+export default App;                           
