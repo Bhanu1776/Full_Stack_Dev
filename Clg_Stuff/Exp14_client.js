11c95902764968c37cdf6f064fef32df71d0fde2
Bhanu Sunka <sunkabhanu281202@gmail.com>
2022-10-09 11:00:02 +0530

    Ip experiment stuff

---
+++ Clg_Stuff/Exp14_client.js
@@ -0,0 +1,22 @@
+var http = require('http');
+// Options to be used by request 
+var options = {
+   host: 'localhost',
+   port: '8081',
+   path: '/Exp4.html'
+};
+// Callback function is used to deal with response
+var callback = function (response) {
+   // Continuously update stream with data
+   var body = '';
+   response.on('data', function (data) {
+      body += data;
+   });
+   response.on('end', function () {
+      // Data received completely.
+      console.log(body);
+   });
+}
+// Make a request to the server
+var req = http.request(options, callback);
+req.end();
